484909dd63bdbf1910912de5a7224fed73b54e15
itsdaxen <fa.rzam.sadegh@gmail.com>
2025-04-21 06:51:23 +0300

    Delete HW3/src/main/java/dev/m3s/programming2 directory

--- HW3/src/main/java/dev/m3s/programming2/Payment.java
+++
@@ -1,5 +0,0 @@
-package dev.m3s.programming2.homework3;
-
-public interface Payment {
-    double calculatePayment();
-}
--- HW3/src/main/java/dev/m3s/programming2/Teacher.java
+++
@@ -1,7 +0,0 @@
-package dev.m3s.programming2.homework3;
-
-public interface Teacher {
-
-    public abstract String getCourses();
-
-}
